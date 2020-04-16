43df309bdfc1ae04151142266550be1d002c8e27
Kaikaine <kairijb@protonmail.com>
2020-04-16 09:30:56 -0500

    add AppBar

--- src/App/index.js
+++ src/App/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./App.css";
 import Welcome from "./WelcomeMessage";
 import styled from "styled-components";
+import AppLayout from "./AppLayout";
+import AppBar from "./AppBar";
 
 function App() {
   return (
-    <div className="App">
+    <AppLayout>
+      <AppBar />
       <Welcome />
-    </div>
+    </AppLayout>
   );
 }
 
