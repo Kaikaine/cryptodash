c4cb04e65eab7a7011042180ea9085140081ca83
Kaikaine <kairijb@protonmail.com>
2020-04-17 19:44:08 -0500

    Added styling to appbar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,18 +1,38 @@
 import React from "react";
 import styled from "styled-components";
 
+const Logo = styled.div`
+  font-size: 2em;
+`;
+
 const Bar = styled.div`
   display: grid;
   grid-template-columns: 180px auto 100px 100px;
+  margin-bottom: 40px;
 `;
+
+const ControlButtonElem = styled.div`
+  cursor: pointer;
+  ${(props) =>
+    props.active ? "text-shadow: 0px 0px 60px #03ff03" : "color:white"}
+`;
+function ControlButton({ name, active }) {
+  return (
+    <ControlButtonElem active={active}>{toProperCase(name)}</ControlButtonElem>
+  );
+}
+
+function toProperCase(lower) {
+  return lower.charAt(0).toUpperCase() + lower.substr(1);
+}
 
 const AppBar = () => {
   return (
     <Bar>
-      <div>CryptoDash</div>
+      <Logo>CryptoDash</Logo>
       <div />
-      <div>Dashboard</div>
-      <div>Settings</div>
+      <ControlButton active name="dashboard" />
+      <ControlButton name="settings" />
     </Bar>
   );
 };
--- src/App/index.js
+++ src/App/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import Welcome from "./WelcomeMessage";
-import styled from "styled-components";
 import AppLayout from "./AppLayout";
 import AppBar from "./AppBar";
 
